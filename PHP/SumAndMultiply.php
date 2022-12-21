f2e59b6e49e1250d72a947c573a1e5472c604a8f
Galya <58034004+Galya777@users.noreply.github.com>
2022-12-21 12:29:21 +0200

    Create SumAndMultiply.php

---
+++ PHP/SumAndMultiply.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$first=5;
+$second=7;
+$third=13;
+$fourth=17;
+$sum= $first+$second+$third+$fourth;
+$multi=$first*$second*$third*$fourth;
+echo $sum, " ";
+echo $multi;
+?> 
+
+</body>
+</html>
